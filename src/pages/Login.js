60927857f2bb98f58b10593bfc09a5c0e05d5921
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2024-12-09 15:40:19 +0800

    add sign up and login function (no jwt)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,13 @@
 import { useState } from 'react';
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import {Card, Form, Button} from 'react-bootstrap'
 
 const Login = () => {
     const [show, setShow] = useState('password')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(null)
+    const navigate = useNavigate()
 
     const handleChange = () =>{
         if(show === 'password'){
@@ -13,6 +17,38 @@ const Login = () => {
         }
     }
 
+    const handleSubmit = async(e) =>{
+        e.preventDefault()
+
+        if(email === ''){
+            console.log('Provide email')
+        }
+        else if(password === ''){
+            console.log('Provide password')
+        }else{
+            const user = {email, password}
+            const response = await fetch('http://127.0.0.1:5000/api/v1/user/login', {
+                method: "POST",
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify(user)
+            })
+
+            const result = await response.json()
+
+            if(!response.ok){
+                setError(result.error)
+                console.log(error)
+            }else{
+                setEmail('')
+                setPassword('')
+                console.log('Login successfull')
+                navigate('/')
+            }
+        }
+    } 
+
     return ( 
         <div className="login">
             <Card className="w-75 mx-auto my-5">
@@ -20,14 +56,23 @@ const Login = () => {
                     <Card.Title className='text-center mb-0 fs-3'>Log in Account</Card.Title>
                     <p className="fs-6 fw-light text-secondary text-center">Welcome back! How have you been?</p>
 
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group className='mb-3 mt-5'>
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder='juandelacruz@email.com'/>
+                            <Form.Control 
+                                type="email" 
+                                placeholder='juandelacruz@email.com'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                         </Form.Group>
                         <Form.Group className='mb-2'>
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type={show}/>
+                            <Form.Control 
+                                type={show}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                         </Form.Group>
                         <Form.Check 
                             type='checkbox' 
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,12 +4,56 @@ import {Card, Form, Button} from 'react-bootstrap'
 
 const Signup = () => {
     const [show, setShow] = useState('password')
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(null)
 
     const handleChange = () =>{
         if(show === 'password'){
             setShow('text')
         }else{
             setShow('password')
+        }
+    }
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault()
+
+        //check if fields are not empty
+        if(name === ''){
+            console.log('Provide name')
+        }
+
+        else if(name === ''){
+            console.log('Provide email')
+        }
+
+        else if(password === ''){
+            console.log('Provide password')
+        }else{
+
+            const data = {email, password, name}
+            const response = await fetch('http://127.0.0.1:5000/api/v1/user/signup', {
+                method: "POST",
+                headers: {
+                    'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+
+            const result = await response.json()
+
+            if(!response.ok){
+                setError(result.error)
+                console.log(error)
+            }else{
+                setEmail('')
+                setName('')
+                setPassword('')
+                console.log('Sign up successfull')
+                //add alert that the sign up is complete
+            }
         }
     }
 
@@ -20,18 +64,33 @@ const Signup = () => {
                     <Card.Title className='text-center mb-0 fs-3'>Create Account</Card.Title>
                     <p className="fs-6 fw-light text-secondary text-center">Hi! Are you ready spice up your meal?</p>
 
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                     <Form.Group className='mb-3 mt-5'>
                             <Form.Label>Name</Form.Label>
-                            <Form.Control type="text" placeholder='Juan Dela Cruz'/>
+                            <Form.Control 
+                                type="text" 
+                                placeholder='Juan Dela Cruz'
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
                         </Form.Group>
                         <Form.Group className='mb-3'>
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder='juandelacruz@email.com'/>
+                            <Form.Control 
+                                type="email" 
+                                placeholder='juandelacruz@email.com'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
                         </Form.Group>
                         <Form.Group className='mb-2'>
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type={show} placeholder='Minimum of 8 characters'/>
+                            <Form.Control
+                                type={show} 
+                                placeholder='Minimum of 8 characters'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
                         </Form.Group>
                         <Form.Check 
                             type='checkbox' 
